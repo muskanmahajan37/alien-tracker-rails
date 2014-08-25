8fb08afee69c6c4f3f3aa642926b9ba0ebc8b860
Eric Johnson <student@epicodus.com>
2014-08-25 10:32:29 -0700

    Create post aliens route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   match('aliens', {:via => :get, :to => 'aliens#index'})
   match('aliens/new', {:via => :get, :to => 'aliens#new'})
+  match('aliens', {:via => :post, :to => 'aliens#create'})
 end
