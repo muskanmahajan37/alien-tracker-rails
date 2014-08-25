52ae50c677e0e6c1b336640f66260e4799b8c8d7
Eric Johnson <student@epicodus.com>
2014-08-25 11:27:05 -0700

    Create edit alien route

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   match('aliens/new', {:via => :get, :to => 'aliens#new'})
   match('aliens', {:via => :post, :to => 'aliens#create'})
   match('aliens/:id', {:via => :get, :to => 'aliens#show'})
+  match('aliens/:id/edit', {:via => :get, :to => 'aliens#edit'})
 end
