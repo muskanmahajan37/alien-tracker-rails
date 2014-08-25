37b1ee496f162c0a8b5390f73cf54a42115a1ea7
Eric Johnson <student@epicodus.com>
2014-08-25 11:02:35 -0700

    Create show alien route

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   match('aliens', {:via => :get, :to => 'aliens#index'})
   match('aliens/new', {:via => :get, :to => 'aliens#new'})
   match('aliens', {:via => :post, :to => 'aliens#create'})
+  match('aliens/:id', {:via => :get, :to => 'aliens#show'})
 end
