1755aad7146904f635e0a52ed647b298fb47f807
Eric Johnson <student@epicodus.com>
2014-08-25 09:34:57 -0700

    Update db schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140825163038) do
+ActiveRecord::Schema.define(version: 20140825163323) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "aliens", force: true do |t|
+  end
+
+  create_table "regions", force: true do |t|
+    t.string "name"
+    t.string "type"
   end
 
 end
