ce04d82a007d29f335a306a132763f6b6ecd818d
Eric <erjohnson.pdx@gmail.com>
2014-08-25 14:14:07 -0700

    Add region_id to sightings migration

---
+++ db/migrate/20140825211248_add_region_sightings.rb
@@ -0,0 +1,5 @@
+class AddRegionSightings < ActiveRecord::Migration
+  def change
+    add_column :sightings, :region_id, :int
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140825171008) do
+ActiveRecord::Schema.define(version: 20140825211248) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20140825171008) do
     t.integer  "alien_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "region_id"
   end
 
 end
