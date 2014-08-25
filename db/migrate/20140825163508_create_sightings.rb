7704c1a1b71b6924304d1b203afbd1510d58611d
Eric Johnson <student@epicodus.com>
2014-08-25 09:36:44 -0700

    Update db schema

--- db/migrate/20140825163508_create_sightings.rb
+++ db/migrate/20140825163508_create_sightings.rb
@@ -1,6 +1,10 @@
 class CreateSightings < ActiveRecord::Migration
   def change
     create_table :sightings do |t|
+      t.integer :latitude
+      t.integer :longitude
+      t.belongs_to :alien
+      t.timestamps
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140825163323) do
+ActiveRecord::Schema.define(version: 20140825163508) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,14 @@ ActiveRecord::Schema.define(version: 20140825163323) do
   create_table "regions", force: true do |t|
     t.string "name"
     t.string "type"
+  end
+
+  create_table "sightings", force: true do |t|
+    t.integer  "latitude"
+    t.integer  "longitude"
+    t.integer  "alien_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
