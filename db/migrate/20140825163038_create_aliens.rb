8f71e5821df88f56557de560c0a8a9a69e07b85b
Eric Johnson <student@epicodus.com>
2014-08-25 09:33:06 -0700

    Generate db schema

--- db/migrate/20140825163038_create_aliens.rb
+++ db/migrate/20140825163038_create_aliens.rb
@@ -1,6 +1,10 @@
 class CreateAliens < ActiveRecord::Migration
   def change
     create_table :aliens do |t|
+      t.string :name
+      t.string :type
+      t.boolean :threat
+      t.belongs_to :region
     end
   end
 end
